8193dfecb577638bf8491f00f25beda1edca78d3
Yeseo0502 <5252yeseo@gmail.com>
2026-04-15 11:32:08 +0900

    TodoListApp li -> TodoItemEmpty, button -> Button, input,label -> Checkbox

---
+++ src/components/Checkbox.jsx
@@ -0,0 +1,9 @@
+export default function CheckBox(props){
+    const { children, id } = props
+    return(
+        <>
+            <input type="checkbox" id={`chk-${id}`} className="todo__check"/>
+            <label htmlFor={`chk-${id}`} className="todo__label">지각 하기</label>
+        </>
+    )
+}
